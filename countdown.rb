f2397a67e876508e9ef521c6321063f6603e782a
Oscar Shoudt <shoudto@gmail.com>
2019-05-01 23:31:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(counter)
+  
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+    sleep(5.0)
+  end
+    return "HAPPY NEW YEAR!"
 end
